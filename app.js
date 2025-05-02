fc3fb99cbc1e3d5d18d600c59015cb1cafc5c3d3
fathima <nabeelaalifathima@gmail.com>
2025-05-02 23:09:20 +0530

    bug/fixed

--- app.js
+++ app.js
@@ -61,11 +61,11 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 //Create Route
 app.post("/listings", wrapAsync(async (req, res, next) => {
@@ -77,30 +77,30 @@ app.post("/listings", wrapAsync(async (req, res, next) => {
 }));
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not found"))
-})
+app.all(/./, (req, res, next) => {
+  next(new ExpressError(404,"Page Not Found"));
+});
 
 app.use((err, req, res, next) => {
   let { statusCode, message } = err
